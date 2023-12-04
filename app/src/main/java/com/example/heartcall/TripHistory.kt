d49ed92f59ddb8fc9f55afe9da058b1050383972
Newton Brian <97920978+lawdneo@users.noreply.github.com>
2023-12-04 16:08:51 +0300

    Update TripHistory

--- app/src/main/java/com/example/heartcall/TripHistory.kt
+++ app/src/main/java/com/example/heartcall/TripHistory.kt
@@ -1,18 +1,18 @@
 package com.example.heartcall
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,20 +26,29 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+val trips = listOf(
+    Trip("Keri Road", "Sunshine Apartments", 4.9f, "7:34 AM", imageResource = R.drawable.mombasa),
+    Trip("Tikka Apartments", "Nairobi West Hospital", 4.9f, "6:30 AM", R.drawable.mombasa),
+
+    // Add more trips here...
+)
 @Composable
-fun TripHistoryApp() {
-    // This is a dummy data list to simulate real trips
-    val trips = listOf(
-        Trip("Keri Road", "Sunshine Apartments", 4.9f, "7:34 AM", imageResource = R.drawable.mombasa),
-        Trip("Tikka Apartments", "Nairobi West Hospital", 4.9f, "6:30 AM", R.drawable.mombasa),
-
-        // Add more trips here...
-    )
-
-    Surface(color = MaterialTheme.colorScheme.background) {
-        LazyColumn {
-            items(trips) { trip ->
-                TripItem(trip)
+fun TripHistoryApp(trips: List<Trip>) {
+    MaterialTheme {
+        Surface {
+            Column {
+                Text(
+                    "Trip History",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp, vertical = 16.dp)
+                )
+                LazyColumn(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    items(trips) { trip ->
+                        TripItem(trip)
+                    }
+                }
             }
         }
     }
@@ -47,38 +56,45 @@ fun TripHistoryApp() {
 
 @Composable
 fun TripItem(trip: Trip) {
-    Row(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        Image(
-            painter = painterResource(id = trip.imageResource),
-            contentDescription = "Location Image",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
-            Text(trip.startLocation, fontWeight = FontWeight.Bold)
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(Icons.Default.Place, contentDescription = "Location")
-                Text(trip.endLocation)
-            }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(Icons.Default.Star, contentDescription = "Rating", tint = Color.Yellow)
-                Text("${trip.rating}", fontWeight = FontWeight.Bold)
-                Text(" - ${trip.time}", style = MaterialTheme.typography.bodySmall)
+        Row(
+            modifier = Modifier
+                .background(color = Color.Gray)
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = trip.imageResource),
+                contentDescription = "Trip Image",
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(end = 16.dp)
+            )
+            Column {
+                Text(trip.startLocation, fontWeight = FontWeight.Bold)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.Place, contentDescription = "Location")
+                    Text(trip.endLocation)
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.Star, contentDescription = "Rating", tint = Color.Yellow)
+                    Text("${trip.rating}", fontWeight = FontWeight.Bold)
+                    Text(" - ${trip.time}", style = MaterialTheme.typography.bodySmall)
+                }
             }
         }
-
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TripHistoryApp()
+    TripHistoryApp(trips)
 }
 
 // Data class to represent a trip
